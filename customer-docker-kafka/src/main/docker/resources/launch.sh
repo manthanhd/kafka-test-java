a45e593925ddfc59ec563ef7752aea59cc9ac7ba
Manthan Dave <manthanhd@live.com>
2016-10-03 21:53:06 +0100

    Separated out docker scripts for kafka and zookeeper. Now they both can be deployed independently.

---
+++ customer-docker-kafka/src/main/docker/resources/launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting Kafka..."
+./kafka/bin/kafka-server-start.sh server.properties
---
+++ customer-docker-zookeeper/src/main/docker/resources/launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting Zookeeper..."
+./zookeeper/bin/zkServer.sh start-foreground
